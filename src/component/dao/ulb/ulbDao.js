e1e4b64d09fcee1ffc461a8b49fb3138c5d7a964
Kaushal <kaushalkantmishra127@gmail.com>
2024-09-14 13:35:22 +0530

    added modification in get  api for ulbs. to get financial progress also

--- src/component/dao/ulb/ulbDao.js
+++ src/component/dao/ulb/ulbDao.js
@@ -5,29 +5,39 @@ const getULBs = async () => {
   try {
     const ulbs = await prisma.$queryRaw`
       SELECT 
-    "ULB".id AS id,
-    "ULB".ulb_name AS ulb_name,
-    "ULB".longitude,
-    "ULB".latitude,
-    FSR.approved_schemes,
-    FSR.fund_release_to_ulbs,
-    FSR.amount,
-    FSR.project_completed,
-    FSR.expenditure,
-    FSR.balance_amount,
-    FSR.financial_progress_in_percentage,
-    FSR.number_of_tender_floated,
-    FSR.tender_not_floated,
-    FSR.work_in_progress
-  FROM 
-    "ULB" 
-  LEFT JOIN 
-    "FinancialSummaryReport" AS FSR
-  ON 
-    "ULB".id = FSR.ulb_id
-  ORDER BY 
-    "ULB".id ASC;
-`;
+        "ULB".id AS id,
+        "ULB".ulb_name AS ulb_name,
+        "ULB".longitude,
+        "ULB".latitude,
+        FSR.approved_schemes,
+        FSR.fund_release_to_ulbs,
+        FSR.amount,
+        FSR.project_completed,
+        FSR.expenditure,
+        FSR.balance_amount,
+        FSR.financial_progress_in_percentage,
+        FSR.number_of_tender_floated,
+        FSR.tender_not_floated,
+        FSR.work_in_progress,
+        SUM(SI.financial_progress) AS total_financial_progress
+      FROM 
+        "ULB" 
+      LEFT JOIN 
+        "FinancialSummaryReport" AS FSR
+      ON 
+        "ULB".id = FSR.ulb_id
+      LEFT JOIN 
+        "Scheme_info" AS SI
+      ON 
+        "ULB".ulb_name = SI.ulb
+      GROUP BY 
+        "ULB".id, FSR.approved_schemes, FSR.fund_release_to_ulbs, FSR.amount, 
+        FSR.project_completed, FSR.expenditure, FSR.balance_amount, 
+        FSR.financial_progress_in_percentage, FSR.number_of_tender_floated, 
+        FSR.tender_not_floated, FSR.work_in_progress
+      ORDER BY 
+        "ULB".id ASC;
+    `;
 
     return ulbs;
   } catch (error) {
