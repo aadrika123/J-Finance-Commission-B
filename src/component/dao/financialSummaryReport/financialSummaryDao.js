b990de1fda29193796cbfcdc5ce8ddc2c37b8ae5
Kaushal <kaushalkantmishra127@gmail.com>
2024-09-26 18:13:38 +0530

    final commit after updating not allocated fund

--- src/component/dao/financialSummaryReport/financialSummaryDao.js
+++ src/component/dao/financialSummaryReport/financialSummaryDao.js
@@ -83,7 +83,7 @@ const fetchFinancialSummaryReport = async (
       SUM(s.approved_project_cost) AS amount,
       SUM(CASE WHEN s.project_completion_status = 'yes' THEN 1 ELSE 0 END) AS project_completed,
       SUM(s.financial_progress) AS expenditure,
-      SUM(s.project_cost - s.financial_progress) AS balance_amount,
+      SUM(s.project_cost) - SUM(s.financial_progress) AS balance_amount,
       AVG(s.financial_progress_in_percentage) AS financial_progress_in_percentage,
       SUM(CASE WHEN s.tender_floated = 'yes' THEN 1 ELSE 0 END) AS number_of_tender_floated,
       SUM(CASE WHEN s.tender_floated = 'no' THEN 1 ELSE 0 END) AS tender_not_floated,
